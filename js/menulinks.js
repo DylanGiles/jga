4d731827da3bc69184de5ad2d4235d7e288aa252
Dylan Giles <dylan.y.giles@gmail.com>
2017-09-06 09:29:53 +1000

    restart

--- js/menulinks.js
+++ js/menulinks.js
@@ -1,24 +1,10 @@
 $('.landing-link').click(function() {
-    $('.landing').animate({
-        marginTop: '-10%'
-    },350)
-    
-    setTimeout(function() {
-        $('.landing').animate({
-            background: 'red'
-        })
+    $('#homeLanding').animate({
+        marginTop: "-50%"
+        
+    })
 
-        $('body').animate({
-            background: 'red'
-        })
-    }, 350)
-    
-    setTimeout(function() {
-        $('.landing').toggleClass('invisible')
-    },350);
-    
-})
-
-$('#contactLandingLink').click(function() {
-    $('#contentWrapper').load('../page/contact/index.html');
+    $('body').animate({
+        backgroundColor: "#ffffff"
+    })
 })
